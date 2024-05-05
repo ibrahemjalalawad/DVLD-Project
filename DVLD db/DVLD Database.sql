51f1d6782245539a3dea73307f1a3edb40bff7ac
ibrahemjalalawad <ibrahemjalal3@gmail.com>
2024-05-06 02:30:23 +0300

    adding the database query

---
+++ DVLD db/DVLD Database.sql
@@ -0,0 +1,594 @@
+USE [DVLD]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Applications](
+	[ApplicationID] [int] IDENTITY(1,1) NOT NULL,
+	[ApplicantPersonID] [int] NOT NULL,
+	[ApplicationDate] [datetime] NOT NULL,
+	[ApplicationTypeID] [int] NOT NULL,
+	[ApplicationStatus] [tinyint] NOT NULL,
+	[LastStatusDate] [datetime] NOT NULL,
+	[PaidFees] [smallmoney] NOT NULL,
+	[CreatedByUserID] [int] NOT NULL,
+ CONSTRAINT [PK_Applications] PRIMARY KEY CLUSTERED 
+(
+	[ApplicationID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Applications] ADD  CONSTRAINT [DF_Applications_ApplicationStatus]  DEFAULT ((1)) FOR [ApplicationStatus]
+GO
+
+ALTER TABLE [dbo].[Applications]  WITH CHECK ADD  CONSTRAINT [FK_Applications_ApplicationTypes] FOREIGN KEY([ApplicationTypeID])
+REFERENCES [dbo].[ApplicationTypes] ([ApplicationTypeID])
+GO
+
+ALTER TABLE [dbo].[Applications] CHECK CONSTRAINT [FK_Applications_ApplicationTypes]
+GO
+
+ALTER TABLE [dbo].[Applications]  WITH CHECK ADD  CONSTRAINT [FK_Applications_People] FOREIGN KEY([ApplicantPersonID])
+REFERENCES [dbo].[People] ([PersonID])
+GO
+
+ALTER TABLE [dbo].[Applications] CHECK CONSTRAINT [FK_Applications_People]
+GO
+
+ALTER TABLE [dbo].[Applications]  WITH CHECK ADD  CONSTRAINT [FK_Applications_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[Applications] CHECK CONSTRAINT [FK_Applications_Users]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'1-New 2-Cancelled 3-Completed' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Applications', @level2type=N'COLUMN',@level2name=N'ApplicationStatus'
+GO
+
+
+USE [DVLD]
+GO
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ApplicationTypes](
+	[ApplicationTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[ApplicationTypeTitle] [nvarchar](150) NOT NULL,
+	[ApplicationFees] [smallmoney] NOT NULL,
+ CONSTRAINT [PK_ApplicationTypes] PRIMARY KEY CLUSTERED 
+(
+	[ApplicationTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ApplicationTypes] ADD  CONSTRAINT [DF_ApplicationTypes_Fees]  DEFAULT ((0)) FOR [ApplicationFees]
+GO
+
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Countries](
+	[CountryID] [int] IDENTITY(1,1) NOT NULL,
+	[CountryName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK__Countrie__10D160BFDBD6933F] PRIMARY KEY CLUSTERED 
+(
+	[CountryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DetainedLicenses](
+	[DetainID] [int] IDENTITY(1,1) NOT NULL,
+	[LicenseID] [int] NOT NULL,
+	[DetainDate] [smalldatetime] NOT NULL,
+	[FineFees] [smallmoney] NOT NULL,
+	[CreatedByUserID] [int] NOT NULL,
+	[IsReleased] [bit] NOT NULL,
+	[ReleaseDate] [smalldatetime] NULL,
+	[ReleasedByUserID] [int] NULL,
+	[ReleaseApplicationID] [int] NULL,
+ CONSTRAINT [PK_DetainedLicenses] PRIMARY KEY CLUSTERED 
+(
+	[DetainID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses] ADD  CONSTRAINT [DF_DetainedLicenses_IsReleased]  DEFAULT ((0)) FOR [IsReleased]
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses]  WITH CHECK ADD  CONSTRAINT [FK_DetainedLicenses_Applications] FOREIGN KEY([ReleaseApplicationID])
+REFERENCES [dbo].[Applications] ([ApplicationID])
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses] CHECK CONSTRAINT [FK_DetainedLicenses_Applications]
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses]  WITH CHECK ADD  CONSTRAINT [FK_DetainedLicenses_Licenses] FOREIGN KEY([LicenseID])
+REFERENCES [dbo].[Licenses] ([LicenseID])
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses] CHECK CONSTRAINT [FK_DetainedLicenses_Licenses]
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses]  WITH CHECK ADD  CONSTRAINT [FK_DetainedLicenses_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses] CHECK CONSTRAINT [FK_DetainedLicenses_Users]
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses]  WITH CHECK ADD  CONSTRAINT [FK_DetainedLicenses_Users1] FOREIGN KEY([ReleasedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[DetainedLicenses] CHECK CONSTRAINT [FK_DetainedLicenses_Users1]
+GO
+
+USE [DVLD]
+GO
+
+/******  ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Drivers](
+	[DriverID] [int] IDENTITY(1,1) NOT NULL,
+	[PersonID] [int] NOT NULL,
+	[CreatedByUserID] [int] NOT NULL,
+	[CreatedDate] [smalldatetime] NOT NULL,
+ CONSTRAINT [PK_Drivers_1] PRIMARY KEY CLUSTERED 
+(
+	[DriverID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Drivers]  WITH CHECK ADD  CONSTRAINT [FK_Drivers_People] FOREIGN KEY([PersonID])
+REFERENCES [dbo].[People] ([PersonID])
+GO
+
+ALTER TABLE [dbo].[Drivers] CHECK CONSTRAINT [FK_Drivers_People]
+GO
+
+ALTER TABLE [dbo].[Drivers]  WITH CHECK ADD  CONSTRAINT [FK_Drivers_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[Drivers] CHECK CONSTRAINT [FK_Drivers_Users]
+GO
+USE [DVLD]
+GO
+
+/******  ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[InternationalLicenses](
+	[InternationalLicenseID] [int] IDENTITY(1,1) NOT NULL,
+	[ApplicationID] [int] NOT NULL,
+	[DriverID] [int] NOT NULL,
+	[IssuedUsingLocalLicenseID] [int] NOT NULL,
+	[IssueDate] [smalldatetime] NOT NULL,
+	[ExpirationDate] [smalldatetime] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreatedByUserID] [int] NOT NULL,
+ CONSTRAINT [PK_InternationalLicenses] PRIMARY KEY CLUSTERED 
+(
+	[InternationalLicenseID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses]  WITH CHECK ADD  CONSTRAINT [FK_InternationalLicenses_Applications] FOREIGN KEY([ApplicationID])
+REFERENCES [dbo].[Applications] ([ApplicationID])
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses] CHECK CONSTRAINT [FK_InternationalLicenses_Applications]
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses]  WITH CHECK ADD  CONSTRAINT [FK_InternationalLicenses_Drivers] FOREIGN KEY([DriverID])
+REFERENCES [dbo].[Drivers] ([DriverID])
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses] CHECK CONSTRAINT [FK_InternationalLicenses_Drivers]
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses]  WITH CHECK ADD  CONSTRAINT [FK_InternationalLicenses_Licenses] FOREIGN KEY([IssuedUsingLocalLicenseID])
+REFERENCES [dbo].[Licenses] ([LicenseID])
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses] CHECK CONSTRAINT [FK_InternationalLicenses_Licenses]
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses]  WITH CHECK ADD  CONSTRAINT [FK_InternationalLicenses_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[InternationalLicenses] CHECK CONSTRAINT [FK_InternationalLicenses_Users]
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[LicenseClasses](
+	[LicenseClassID] [int] IDENTITY(1,1) NOT NULL,
+	[ClassName] [nvarchar](50) NOT NULL,
+	[ClassDescription] [nvarchar](500) NOT NULL,
+	[MinimumAllowedAge] [tinyint] NOT NULL,
+	[DefaultValidityLength] [tinyint] NOT NULL,
+	[ClassFees] [smallmoney] NOT NULL,
+ CONSTRAINT [PK_LicenseClasses] PRIMARY KEY CLUSTERED 
+(
+	[LicenseClassID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[LicenseClasses] ADD  CONSTRAINT [DF_LicenseClasses_Age]  DEFAULT ((18)) FOR [MinimumAllowedAge]
+GO
+
+ALTER TABLE [dbo].[LicenseClasses] ADD  CONSTRAINT [DF_LicenseClasses_DefaultPeriodLength]  DEFAULT ((1)) FOR [DefaultValidityLength]
+GO
+
+ALTER TABLE [dbo].[LicenseClasses] ADD  CONSTRAINT [DF_LicenseClasses_ClassFees]  DEFAULT ((0)) FOR [ClassFees]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Minmum age allowed to apply for this license' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LicenseClasses', @level2type=N'COLUMN',@level2name=N'MinimumAllowedAge'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'How many years the licesnse will be valid.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'LicenseClasses', @level2type=N'COLUMN',@level2name=N'DefaultValidityLength'
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Licenses](
+	[LicenseID] [int] IDENTITY(1,1) NOT NULL,
+	[ApplicationID] [int] NOT NULL,
+	[DriverID] [int] NOT NULL,
+	[LicenseClass] [int] NOT NULL,
+	[IssueDate] [datetime] NOT NULL,
+	[ExpirationDate] [datetime] NOT NULL,
+	[Notes] [nvarchar](500) NULL,
+	[PaidFees] [smallmoney] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[IssueReason] [tinyint] NOT NULL,
+	[CreatedByUserID] [int] NOT NULL,
+ CONSTRAINT [PK_Licenses] PRIMARY KEY CLUSTERED 
+(
+	[LicenseID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Licenses] ADD  CONSTRAINT [DF_Licenses_IsActive]  DEFAULT ((1)) FOR [IsActive]
+GO
+
+ALTER TABLE [dbo].[Licenses] ADD  CONSTRAINT [DF_Licenses_IssueReason]  DEFAULT ((1)) FOR [IssueReason]
+GO
+
+ALTER TABLE [dbo].[Licenses]  WITH CHECK ADD  CONSTRAINT [FK_Licenses_Applications] FOREIGN KEY([ApplicationID])
+REFERENCES [dbo].[Applications] ([ApplicationID])
+GO
+
+ALTER TABLE [dbo].[Licenses] CHECK CONSTRAINT [FK_Licenses_Applications]
+GO
+
+ALTER TABLE [dbo].[Licenses]  WITH CHECK ADD  CONSTRAINT [FK_Licenses_Drivers] FOREIGN KEY([DriverID])
+REFERENCES [dbo].[Drivers] ([DriverID])
+GO
+
+ALTER TABLE [dbo].[Licenses] CHECK CONSTRAINT [FK_Licenses_Drivers]
+GO
+
+ALTER TABLE [dbo].[Licenses]  WITH CHECK ADD  CONSTRAINT [FK_Licenses_LicenseClasses] FOREIGN KEY([LicenseClass])
+REFERENCES [dbo].[LicenseClasses] ([LicenseClassID])
+GO
+
+ALTER TABLE [dbo].[Licenses] CHECK CONSTRAINT [FK_Licenses_LicenseClasses]
+GO
+
+ALTER TABLE [dbo].[Licenses]  WITH CHECK ADD  CONSTRAINT [FK_Licenses_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[Licenses] CHECK CONSTRAINT [FK_Licenses_Users]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'1-FirstTime, 2-Renew, 3-Replacement for Damaged, 4- Replacement for Lost.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Licenses', @level2type=N'COLUMN',@level2name=N'IssueReason'
+GO
+
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[LocalDrivingLicenseApplications](
+	[LocalDrivingLicenseApplicationID] [int] IDENTITY(1,1) NOT NULL,
+	[ApplicationID] [int] NOT NULL,
+	[LicenseClassID] [int] NOT NULL,
+ CONSTRAINT [PK_DrivingLicsenseApplications] PRIMARY KEY CLUSTERED 
+(
+	[LocalDrivingLicenseApplicationID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[LocalDrivingLicenseApplications]  WITH CHECK ADD  CONSTRAINT [FK_DrivingLicsenseApplications_Applications] FOREIGN KEY([ApplicationID])
+REFERENCES [dbo].[Applications] ([ApplicationID])
+GO
+
+ALTER TABLE [dbo].[LocalDrivingLicenseApplications] CHECK CONSTRAINT [FK_DrivingLicsenseApplications_Applications]
+GO
+
+ALTER TABLE [dbo].[LocalDrivingLicenseApplications]  WITH CHECK ADD  CONSTRAINT [FK_DrivingLicsenseApplications_LicenseClasses] FOREIGN KEY([LicenseClassID])
+REFERENCES [dbo].[LicenseClasses] ([LicenseClassID])
+GO
+
+ALTER TABLE [dbo].[LocalDrivingLicenseApplications] CHECK CONSTRAINT [FK_DrivingLicsenseApplications_LicenseClasses]
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[People](
+	[PersonID] [int] IDENTITY(1,1) NOT NULL,
+	[NationalNo] [nvarchar](20) NOT NULL,
+	[FirstName] [nvarchar](20) NOT NULL,
+	[SecondName] [nvarchar](20) NOT NULL,
+	[ThirdName] [nvarchar](20) NULL,
+	[LastName] [nvarchar](20) NOT NULL,
+	[DateOfBirth] [datetime] NOT NULL,
+	[Gendor] [tinyint] NOT NULL,
+	[Address] [nvarchar](500) NOT NULL,
+	[Phone] [nvarchar](20) NOT NULL,
+	[Email] [nvarchar](50) NULL,
+	[NationalityCountryID] [int] NOT NULL,
+	[ImagePath] [nvarchar](250) NULL,
+ CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED 
+(
+	[PersonID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[People] ADD  CONSTRAINT [DF_People_Gendor]  DEFAULT ((0)) FOR [Gendor]
+GO
+
+ALTER TABLE [dbo].[People]  WITH CHECK ADD  CONSTRAINT [FK_People_Countries1] FOREIGN KEY([NationalityCountryID])
+REFERENCES [dbo].[Countries] ([CountryID])
+GO
+
+ALTER TABLE [dbo].[People] CHECK CONSTRAINT [FK_People_Countries1]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'0 Male , 1 Femail' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'People', @level2type=N'COLUMN',@level2name=N'Gendor'
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TestAppointments](
+	[TestAppointmentID] [int] IDENTITY(1,1) NOT NULL,
+	[TestTypeID] [int] NOT NULL,
+	[LocalDrivingLicenseApplicationID] [int] NOT NULL,
+	[AppointmentDate] [smalldatetime] NOT NULL,
+	[PaidFees] [smallmoney] NOT NULL,
+	[CreatedByUserID] [int] NOT NULL,
+	[IsLocked] [bit] NOT NULL,
+	[RetakeTestApplicationID] [int] NULL,
+ CONSTRAINT [PK_TestAppointments] PRIMARY KEY CLUSTERED 
+(
+	[TestAppointmentID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TestAppointments] ADD  CONSTRAINT [DF_TestAppointments_AppointmentLocked]  DEFAULT ((0)) FOR [IsLocked]
+GO
+
+ALTER TABLE [dbo].[TestAppointments]  WITH CHECK ADD  CONSTRAINT [FK_TestAppointments_Applications] FOREIGN KEY([RetakeTestApplicationID])
+REFERENCES [dbo].[Applications] ([ApplicationID])
+GO
+
+ALTER TABLE [dbo].[TestAppointments] CHECK CONSTRAINT [FK_TestAppointments_Applications]
+GO
+
+ALTER TABLE [dbo].[TestAppointments]  WITH CHECK ADD  CONSTRAINT [FK_TestAppointments_LocalDrivingLicenseApplications] FOREIGN KEY([LocalDrivingLicenseApplicationID])
+REFERENCES [dbo].[LocalDrivingLicenseApplications] ([LocalDrivingLicenseApplicationID])
+GO
+
+ALTER TABLE [dbo].[TestAppointments] CHECK CONSTRAINT [FK_TestAppointments_LocalDrivingLicenseApplications]
+GO
+
+ALTER TABLE [dbo].[TestAppointments]  WITH CHECK ADD  CONSTRAINT [FK_TestAppointments_TestTypes] FOREIGN KEY([TestTypeID])
+REFERENCES [dbo].[TestTypes] ([TestTypeID])
+GO
+
+ALTER TABLE [dbo].[TestAppointments] CHECK CONSTRAINT [FK_TestAppointments_TestTypes]
+GO
+
+ALTER TABLE [dbo].[TestAppointments]  WITH CHECK ADD  CONSTRAINT [FK_TestAppointments_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[TestAppointments] CHECK CONSTRAINT [FK_TestAppointments_Users]
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Tests](
+	[TestID] [int] IDENTITY(1,1) NOT NULL,
+	[TestAppointmentID] [int] NOT NULL,
+	[TestResult] [bit] NOT NULL,
+	[Notes] [nvarchar](500) NULL,
+	[CreatedByUserID] [int] NOT NULL,
+ CONSTRAINT [PK_Tests] PRIMARY KEY CLUSTERED 
+(
+	[TestID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Tests]  WITH CHECK ADD  CONSTRAINT [FK_Tests_TestAppointments] FOREIGN KEY([TestAppointmentID])
+REFERENCES [dbo].[TestAppointments] ([TestAppointmentID])
+GO
+
+ALTER TABLE [dbo].[Tests] CHECK CONSTRAINT [FK_Tests_TestAppointments]
+GO
+
+ALTER TABLE [dbo].[Tests]  WITH CHECK ADD  CONSTRAINT [FK_Tests_Users] FOREIGN KEY([CreatedByUserID])
+REFERENCES [dbo].[Users] ([UserID])
+GO
+
+ALTER TABLE [dbo].[Tests] CHECK CONSTRAINT [FK_Tests_Users]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'0 - Fail 1-Pass' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Tests', @level2type=N'COLUMN',@level2name=N'TestResult'
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TestTypes](
+	[TestTypeID] [int] IDENTITY(1,1) NOT NULL,
+	[TestTypeTitle] [nvarchar](100) NOT NULL,
+	[TestTypeDescription] [nvarchar](500) NOT NULL,
+	[TestTypeFees] [smallmoney] NOT NULL,
+ CONSTRAINT [PK_TestTypes] PRIMARY KEY CLUSTERED 
+(
+	[TestTypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+USE [DVLD]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[PersonID] [int] NOT NULL,
+	[UserName] [nvarchar](20) NOT NULL,
+	[Password] [nvarchar](20) NOT NULL,
+	[IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_People] FOREIGN KEY([PersonID])
+REFERENCES [dbo].[People] ([PersonID])
+GO
+
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_People]
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
